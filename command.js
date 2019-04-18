3f831d138b421797803370394d83759c44a4c42d
Jose Miguel Lueiza Valenzuela <jose.lueiza@latam.com>
2019-04-18 14:30:53 -0400

    first commit

---
+++ command.js
@@ -0,0 +1,28 @@
+const { text } = require("micro");
+const { parse } = require("querystring");
+const { apply } = require("./lib/funcs");
+module.exports = async (req, res) => {
+    // Parse code received through req
+    const body = parse(await text(req));
+    let result, attachments;
+    try {
+        // Pass code to function imported through eval
+        result = await commandController(body.text)
+    } catch (error) {
+    // Capture any errors
+    result = error.message;
+    attachments = [{ text: error.stack }];
+    }
+
+    const message = "`" + body.text + "`: " + result;
+    const response_type = "in_channel";
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ response_type, text: message, attachments }));
+
+};
+
+const commandController = (text) => {
+    return apply(text)
+}
+
